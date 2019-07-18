c5eee014e9b8b2661a6468bf4a8b8b5a9661de1f
Unknown <terry@lowerytech.net>
2019-07-18 12:32:06 -0500

    added login controller

---
+++ src/main/java/net/lowerytech/web/contollers/LoginController.java
@@ -0,0 +1,28 @@
+package net.lowerytech.web.contollers;
+
+import net.lowerytech.commands.LoginCommand;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("login")
+    public String loginForm(Model model){
+        model.addAttribute("loginForm", new LoginCommand ());
+        return "loginform";
+    }
+
+    @RequestMapping(value = "/dologin", method = RequestMethod.POST)
+    public String doLogin(@Valid LoginCommand loginCommand, BindingResult bindingResult){
+        if(bindingResult.hasErrors ()) {
+            return "loginform";
+        }
+        return "logincomplete";
+    }
+}
